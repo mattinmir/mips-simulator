cf605177f67093b44d5aad8c0ccd0c12ccd7ac44
mattinmir <mattinmir@gmail.com>
2014-10-24 11:01:46 +0100

    small update

--- src/mm5213/test_mips.cpp
+++ src/mm5213/test_mips.cpp
@@ -1,46 +1,65 @@
 #include "mips_test.h"
+#include "mips_cpu_safe.h"
+#include <assert.h>   
 
 int main()
 {
-	mips_mem_h mem=mips_mem_create_ram(
+	mips_mem_h mem = mips_mem_create_ram(
 		1<<20,
 		4
 		);
 
-	mips_cpu_h cpu=mips_cpu_create(mem);
+	mips_cpu_h cpu = mips_cpu_create(mem);
 
 	mips_test_begin_suite();
 
-	int testId=mips_test_begin_test("and");
-	int passed=0;
+	uint32_t address = 0;
+
+	int testId = mips_test_begin_test("and");
+	int passed = 0;
 
 	
 	mips_error err = mips_cpu_set_register(cpu, 8, 0x0000FFFFul);
-	if(err==0)
+	if(!err)
 		err = mips_cpu_set_register(cpu, 9, 0x00FFFF00ul);
+	//if (!err)
+	//{
+	//	uint32_t* reg8 = new uint32_t;
+	//	uint32_t* reg9 = new uint32_t;
 
-	// and $10, $8, $9
+	//	err = mips_cpu_set_register(cpu, 10, *reg8 & *reg9);
 
+	//	delete reg8, reg9;
+	//}
+	
 	// TODO : work out the bit-wise encoding for the instruction.
+	if (!err)
+	{
+		uint8_t encoding[4] = { 0x24, 0x50, 0x09, 0x01 }; // and $10, $8, $9 : 0000 0001 0000 1001 0101 0000 0010 0100 (big endian)
 
-	// TODO : Write it into mempory at a known address
-
+		// TODO : Write it into mempory at a known address
+		err = mips_mem_write(mem, address, 4, encoding);
+	}
 	// TODO : Make sure the program-counter is at that address
+	
+	address += 4;
 
-	if(err==0)
+	if(!err)
 		err=mips_cpu_step(cpu);
 
 	uint32_t got;
-	if(err==0)
+	if(!err)
 		err = (mips_error)(err | mips_cpu_get_register(cpu, 10, &got));
 
 	passed = (err == mips_Success) && (got==0x0000FF00);
 
 	mips_test_end_test(testId, passed, NULL);
 
+
+
 	mips_test_end_suite();
 
-	mips_cpu_free(cpu);
+	mips_cpu_free_safe(cpu);
 	mips_mem_free(mem);
 
 	return 0;
